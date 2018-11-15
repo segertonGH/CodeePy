78c50de1f125d9a5c17830dffa35b4194eb188db
Marita Fitzgerald <18976492@students.latrobe.edu.au>
2018-11-15 12:59:03 +1100

    Add new setup data

--- codeepy/setup.py
+++ codeepy/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="codeepy",
-    version="0.0.8",
+    version="0.0.9",
     author="Marita Fitzgerald",
     author_email="maritafitzgerald@gmail.com",
     description="A library for the Codee Robot",
