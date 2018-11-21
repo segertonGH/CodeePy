95b7e4d07d54e68f84992e7f9a64cc579e3fe60d
Marita Fitzgerald <18976492@students.latrobe.edu.au>
2018-11-21 14:39:30 +1100

    Add Speech recognition & Gesture Sample

--- examples/codeepy_examples.py
+++ examples/codeepy_examples.py
@@ -7,6 +7,7 @@ help(CodeePy)
 CodeePy.get_com_ports()
 
 # create CodeePy Instance and open connection (Change COM port to your own com port value)
+# You can use your robots name in lieu of board ie <Your Robots Name> = CodeePy('<Your COM Port>')
 board = CodeePy('COM12')
 
 # say text
